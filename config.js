224103d37ed23711182d3a5098baf2aa60eecc48
ThatcherC <j.thatcher.c@gmail.com>
2014-11-25 19:10:23 -0500

    Added a config file and logging

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = 
+{
+	"logRequests":true,			//Allows logging of http requests (IP, time)
+	"requestLogPath":"logs/requests.log",	//Where to log requests
+	"logParams":true,			//Allows logging of model parameters
+	"paramLogPath":"logs/params.log"		//Where to log parameters
+}
