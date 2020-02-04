d34470988005dd2f6733b2922f7705ab3bba3ee7
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2020-02-03 22:34:05 -0500

    Working C stltest

---
+++ src/stltest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "STLWriter.h"
+
+// gcc stltest.c STLWriter.c -o stltest -lm
+
+int main(){
+  struct _vect3 o = {0,0,0};
+  struct _vect3 x = {1,0,0};
+  struct _vect3 y = {0,1,0};
+  struct _vect3 z = {0,0,1};
+
+  triangle xy = createTriangle(o,y,x);
+  triangle yz = createTriangle(o,z,y);
+  triangle zx = createTriangle(o,x,z);
+  triangle xyz = createTriangle(x,y,z);
+
+  FILE *stl = fopen("corner.stl", "w");
+
+  startSTLfile(stl, 4);
+
+  addTriangle(stl, xy);
+  addTriangle(stl, yz);
+  addTriangle(stl, zx);
+  addTriangle(stl, xyz);
+
+  fclose(stl);
+}
