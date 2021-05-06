b47ad04d6ba99e015d64193c2152994accb6aafd
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2021-05-06 21:47:24 +0000

    Improved handling of Google Maps API key

---
+++ example.include-maps.js
@@ -0,0 +1,7 @@
+// use Javascript Dynamic Script Loading 
+var script = document.createElement('script');
+script.type= 'text/javascript';
+script.src = "https://maps.googleapis.com/maps/api/js?key=GOOGLE_API_KEY&callback=initializeMap";
+script.async=true;
+script.defer=true;
+document.head.appendChild(script)
