3081a1da3a1dbe2b228ce791c3c2c41d16a31ad7
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2024-05-22 08:04:19 -0400

    Successfully log input file info

--- gelevstl.c
+++ gelevstl.c
@@ -16,7 +16,29 @@ int main(int argc, const char* argv[])
     hDataset = GDALOpen( pszFilename, eAccess );
     if( hDataset == NULL )
     {
-//        ...; // handle error
+        printf("Unable to open file %s\n", pszFilename);
+		return 1;
     }
+
+    GDALDriverH   hDriver;
+double        adfGeoTransform[6];
+hDriver = GDALGetDatasetDriver( hDataset );
+printf( "Driver: %s/%s\n",
+        GDALGetDriverShortName( hDriver ),
+        GDALGetDriverLongName( hDriver ) );
+printf( "Size is %dx%dx%d\n",
+        GDALGetRasterXSize( hDataset ),
+        GDALGetRasterYSize( hDataset ),
+        GDALGetRasterCount( hDataset ) );
+if( GDALGetProjectionRef( hDataset ) != NULL )
+    printf( "Projection is `%s'\n", GDALGetProjectionRef( hDataset ) );
+if( GDALGetGeoTransform( hDataset, adfGeoTransform ) == CE_None )
+{
+    printf( "Origin = (%.6f,%.6f)\n",
+            adfGeoTransform[0], adfGeoTransform[3] );
+    printf( "Pixel Size = (%.6f,%.6f)\n",
+            adfGeoTransform[1], adfGeoTransform[5] );
+}
+
     return 0;
 }
